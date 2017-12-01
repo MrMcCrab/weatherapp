634f8a5a2584b1bbfb5ff92899e39df5b0d6993e
MrMcCrab <>
2017-12-01 20:53:06 +0200

    Initial commit

---
+++ testdata.js
@@ -0,0 +1,42 @@
+{
+  "coord": {
+    "lon": 24.94,
+    "lat": 60.17
+  },
+  "weather": [
+    {
+      "id": 600,
+      "main": "Snow",
+      "description": "light snow",
+      "icon": "13n"
+    }
+  ],
+  "base": "stations",
+  "main": {
+    "temp": 273.15,
+    "pressure": 1003,
+    "humidity": 100,
+    "temp_min": 273.15,
+    "temp_max": 273.15
+  },
+  "visibility": 2800,
+  "wind": {
+    "speed": 6.2,
+    "deg": 30
+  },
+  "clouds": {
+    "all": 75
+  },
+  "dt": 1512136200,
+  "sys": {
+    "type": 1,
+    "id": 5019,
+    "message": 0.153,
+    "country": "FI",
+    "sunrise": 1512111475,
+    "sunset": 1512134445
+  },
+  "id": 658225,
+  "name": "Helsinki",
+  "cod": 200
+}
